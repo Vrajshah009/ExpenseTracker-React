2cafccd888ad1c08a10e90fd9ca914daeb86a934
Vraj shah <vrajshah025@gmail.com>
2025-02-07 16:03:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+//import Aboutus from "./components/aboutus";
+//import Navbar from "./components/Navbar";
+//import Textform from "./components/Textform"
+//import TodoList from "./todolist";
+import Expencetracker from "./Expencetracker";
+
+function App() {
+  return (
+    <>
+    <Expencetracker></Expencetracker>
+    </>
+  );
+}
+
+export default App;
